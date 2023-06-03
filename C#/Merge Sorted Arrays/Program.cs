a236f4d8cf38c5c2516967ad01bd5b1e196d4c38
CoreyJiaJinHe <135453271+CoreyJiaJinHe@users.noreply.github.com>
2023-06-03 15:59:38 -0400

    Add files via upload

---
+++ C#/Merge Sorted Arrays/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Numerics;
+
+public class Class1
+{
+
+    public static void Main()
+    {
+        int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+        int m = 3;
+        int[] nums2 = { 2, 5, 6 };
+        int n = 3;
+        merge(nums1, m, nums2, n);
+
+        foreach (var x in nums1)
+        {
+            Console.WriteLine(x.ToString());
+        }
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (j >= 0)
+        {
+            if (i >= 0 && nums1[i] > nums2[j])
+            {
+                nums1[k--] = nums1[i--];
+            }
+            else
+            {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+
+
+
+
+
+}
